00b75192838ff653f37bc83a54fab727dd760aad
Nick Guy <nguysur@gmail.com>
2015-07-04 02:30:15 -0600

    Added concatenation of flight hours to flthours.py

--- project/flthours.py
+++ project/flthours.py
@@ -40,20 +40,20 @@ class FltHrs(object):
 
         # Set date formats to be used with datetime
         self.d_fmt = "%Y-%m-%d %H:%M"
-        
+
         # Use passed arguments
         self.project = args.project
         self.hours = args.hours
-        
+
         # Set the URL to pull data
         self.url = "http://flights.uwyo.edu/projects/" + self.project + "/"
-        
+
         # Create an date-time array for project hours
         self.start = datetime.strptime(args.start, "%Y-%m-%d")
         self.end = datetime.strptime(args.end, "%Y-%m-%d")
         #Calculate the number of days in project
         self.projdays = (self.end - self.start).days
-        
+
         rng = pd.date_range(args.start, periods=self.projdays, freq='D')
         self.hrsrng = pd.Series(np.linspace(0., self.hours, num=self.projdays, endpoint=True), \
                                   index=rng)
@@ -62,7 +62,7 @@ class FltHrs(object):
         '''Function to return flight data from web'''
         # Load in the URL and parse
         self.df = pd.read_html(self.url, header=1, parse_dates=True, index_col=0)
-        
+
         # Sum the research, calibration, and test hours
         self.reshrs = self.df[1]['Hours'].sum()
         self.calhrs = self.df[2]['Hours'].sum()
@@ -72,11 +72,10 @@ class FltHrs(object):
         self.fieldhrs = self.reshrs + self.calhrs
 
         # Create arrays for research and calibration flights
-        self.resflt = self.df[1]
+        self.resflt = pd.concat([self.df[1],self.df[2]])
         self.calflt = self.df[2]
         self.resflt.sort(ascending=True, inplace=True)
         self.calflt.sort(ascending=True, inplace=True)
-        
 
     def write_json(self, loc=None):
         '''Create a JSON text output file with total flight hours to date'''
@@ -86,19 +85,19 @@ class FltHrs(object):
         else:
             webpath = os.sep.join([loc,'flthours.js'])
         nowdate = datetime.now()
-        
+
         # Create string
         strout = "var flthours = 'As of %s, %.1f out of %d research hours were flown, "\
                  "%.1f remain.';\n"%\
                  (nowdate.strftime("%b %d, %Y"), self.fieldhrs, \
                   self.hours, self.hours-self.fieldhrs)
-        
+
         # Open and write json file
         js = open(webpath, 'w')
         js.write(strout)
         js.write("var testhours = 'Test: %.1f';"%(self.testhrs))
         js.close()
-        
+
     def plot_flight_hours(self, width=500, height=500, \
                           ln_wd=3, sz=10, fDir=".", fName='uwka_flthrs'):
         '''
@@ -119,14 +118,14 @@ class FltHrs(object):
         fName : str
             Output filename [default 'meteogram']
         '''
-        
+
         filepath = os.sep.join([fDir, fName + "_" + self.project +".html"])
         pagetitle = "UWKA Flight Hours"
-        
+
         output_file(filepath, title=pagetitle)
         TOOLS = "pan,wheel_zoom,box_zoom,reset,save,hover"
         TITLE = "UWKA %s: Flight Hours"%(self.project)
-        
+
         # Create a data source for 
         sourceres = ColumnDataSource(
                   data=dict(
@@ -135,7 +134,7 @@ class FltHrs(object):
                   Hours=self.resflt['Hours'].values,
                        )
                  )
-                 
+
         sourcecal = ColumnDataSource(
                   data=dict(
                   Date=self.calflt.index.format(),
@@ -143,21 +142,21 @@ class FltHrs(object):
                   Hours=self.calflt['Hours'].values,
                        )
                  )
-        
+
         self.hrsrng.plot()
         p = figure(x_axis_type = "datetime", tools=TOOLS, width=width, height=height)
         p.line(self.hrsrng.index.values, self.hrsrng.values, color='black', \
                 legend='Allocated', line_width=ln_wd)
         p.circle(self.resflt.index, self.resflt['Hours'].cumsum().values, color='red', \
                   legend='Cumulative', size=sz, source=sourceres)
-        p.circle(self.calflt.index, self.calflt['Hours'].cumsum().values, color='blue', \
-                  legend='Cumulative', size=sz, source=sourcecal)
+#        p.circle(self.calflt.index, self.calflt['Hours'].cumsum().values, color='blue', \
+#                  legend='Cumulative', size=sz, source=sourcecal)
         p.title = TITLE
         p.grid.grid_line_alpha=0.3
         p.xaxis.axis_label = 'Date'
         p.yaxis.axis_label = 'Hours'
         p.legend.orientation = "top_left"
-        
+
         hover = p.select(dict(type=HoverTool))
         hover.tooltips = OrderedDict([
             ("Cum. Hours", "@y"),
@@ -166,10 +165,10 @@ class FltHrs(object):
             ("Hours","@Hours"),
             ("index", "$index"),
         ])
-        
+
         show(p)
         #save(obj=p, filename=filepath, title=pagetitle)
-        
+
 #####################
 ## RUN THE PROGRAM ##
 #####################
@@ -180,7 +179,7 @@ if __name__ == '__main__':
     To run:
         $ flthours.py
     """
-    
+
     # Define the command line structure
     parser = argparse.ArgumentParser(description="Create a flight hours plot for project")
 
@@ -193,13 +192,13 @@ if __name__ == '__main__':
                         "Optional arguments below can be passed"
                         "to override the global variables."
                         " "))
-                       
+
     igroup.add_argument('-p', '--project', type=str, default=None, 
                               help='project (e.g. pecan15)')
-                       
+
     igroup.add_argument('-t', '--hours', type=str, default=None, 
                               help='project (e.g. 120)')
-                       
+
     igroup.add_argument('-s', '--start', type=str, default=None, 
                               help='Start date to search (e.g. "2015-6-1")')
                        
@@ -216,15 +215,15 @@ if __name__ == '__main__':
         args.start = FIRSTDATE
     if args.end is None:
         args.end = LASTDATE
-    
+
     # Start the Metar class
     flight = FltHrs(args)
-    
+
     # Load the data
     flight.get_web_data()
-    
+
     # Create a json file
     flight.write_json(loc=".")
-    
+
     # Plot a meteogram
     flight.plot_flight_hours(fDir=".")
